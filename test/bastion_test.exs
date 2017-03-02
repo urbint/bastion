ea67d1d90f569a7c75e13ed2714fe861381d49b1
Russell Matney <russell.matney@gmail.com>
2017-03-02 17:25:53 -0500

    refactor(test): tests refactored

--- test/bastion_test.exs
+++ test/bastion_test.exs
@@ -6,77 +6,7 @@ defmodule BastionTest do
 
   import Bastion
 
-  describe "required_scopes/2" do
-    setup do
-      private_query = """
-        query TestQuery {
-          users:private_users {
-            name, id
-          }
-        }
-        """
-
-      public_query = """
-        query TestQuery {
-          users:public_users {
-            name, id
-          }
-        }
-        """
-
-      {:ok, private_query: private_query, public_query: public_query}
-    end
-
-    test "works as expected", %{private_query: private_query, public_query: public_query} do
-      assert_query_returns_data(private_query)
-      assert_query_returns_data(public_query)
-    end
-
-    test "returns scopes specified by the schema and query request metadata",
-      %{private_query: private_query, public_query: public_query}
-    do
-      assert required_scopes(TestSchema, private_query) == {:ok, [:admin]}
-      assert required_scopes(TestSchema, public_query) == :no_scopes_required
-    end
-
-    test "authorizes :admin for :admin scopes",
-      %{private_query: private_query, public_query: public_query}
-    do
-      #private query
-      assert :ok = authorize(TestSchema, private_query, [:admin])
-      assert {:error, _} = authorize(TestSchema, private_query, [:user])
-      assert {:error, _} = authorize(TestSchema, private_query, [])
-
-      # public query
-      assert :ok = authorize(TestSchema, public_query, [:admin])
-      assert :ok = authorize(TestSchema, public_query, [:user])
-      assert :ok = authorize(TestSchema, public_query, [])
-    end
-
-    defp assert_query_returns_data(query) do
-      result =
-        query
-        |> Absinthe.run(TestSchema)
-        |> case do
-          {:ok, result} ->
-            result
-        end
-
-      assert result ==
-        %{data:
-          %{"users" => [
-            %{"id" => 7, "name" => "Luke Skywalker"},
-            %{"id" => 999999, "name" => "Darth Vader"}
-          ]}
-        }
-    end
-
-  end
-
-
   defmodule TestSchema do
-    @moduledoc false
-
     use Absinthe.Schema
 
     object :private_user do
@@ -92,11 +22,8 @@ defmodule BastionTest do
     end
 
     query do
-      field :private_users, list_of(:private_user),
-        resolve: &resolver_fn/3
-
-      field :public_users, list_of(:public_user),
-        resolve: &resolver_fn/3
+      field :private_users, list_of(:private_user), resolve: &resolver_fn/3
+      field :public_users, list_of(:public_user), resolve: &resolver_fn/3
     end
 
     defp resolver_fn(_parent, _args, _blueprint) do
@@ -106,4 +33,83 @@ defmodule BastionTest do
       ]}
     end
   end
+
+  defp assert_query_returns_data(query) do
+    result =
+      query
+      |> Absinthe.run(TestSchema)
+      |> case do
+        {:ok, result} ->
+          result
+      end
+
+    assert result ==
+      %{data:
+        %{"users" => [
+          %{"id" => 7, "name" => "Luke Skywalker"},
+          %{"id" => 999999, "name" => "Darth Vader"}
+        ]}
+      }
+  end
+
+  describe "required_scopes/2 and authorize/3" do
+    setup %{test_case: test_case} do
+      basic_public = """
+        query TestQuery {
+          users:public_users {
+            name, id
+          }
+        }
+        """
+
+      basic_private = """
+        query TestQuery {
+          users:private_users {
+            name, id
+          }
+        }
+        """
+
+      context =
+        case test_case do
+          :basic ->
+            %{public_query: basic_public, private_query: basic_private}
+        end
+
+      {:ok, context: context}
+    end
+
+    @tag test_case: :basic
+    test "works as expected", %{context: context} do
+      %{private_query: private_query, public_query: public_query} = context
+
+      assert_query_returns_data(private_query)
+      assert_query_returns_data(public_query)
+    end
+
+    @tag test_case: :basic
+    test "returns required scopes for requested objects", %{context: context} do
+      %{private_query: private_query, public_query: public_query} = context
+
+      assert required_scopes(TestSchema, private_query) == {:ok, [:admin]}
+      assert required_scopes(TestSchema, public_query) == :no_scopes_required
+    end
+
+    @tag test_case: :basic
+    test "authorizes :admin for :admin scopes", %{context: context} do
+      %{private_query: private_query, public_query: public_query} = context
+
+      #private query
+      assert :ok = authorize(TestSchema, private_query, [:admin])
+      assert {:error, _} = authorize(TestSchema, private_query, [:user])
+      assert {:error, _} = authorize(TestSchema, private_query, [])
+
+      # public query
+      assert :ok = authorize(TestSchema, public_query, [:admin])
+      assert :ok = authorize(TestSchema, public_query, [:user])
+      assert :ok = authorize(TestSchema, public_query, [])
+    end
+
+  end
+
 end
